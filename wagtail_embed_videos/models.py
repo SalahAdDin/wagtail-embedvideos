96771068141933e526ac6f22a9a52208205bdef3
José Luis <alagunasalahaddin@live.com>
2018-01-27 03:48:37 +0300

    Update models.py

--- wagtail_embed_videos/models.py
+++ wagtail_embed_videos/models.py
@@ -105,7 +105,7 @@ class AbstractEmbedVideo(CollectionMember, index.Indexed, models.Model):
         blank=True,
         editable=False,
         verbose_name=_('Uploader'),
-        on_delete=models.SET_NULL,
+        on_delete=models.CASCADE,
     )
 
     tags = TaggableManager(help_text=None, blank=True, verbose_name=_('Tags'))
