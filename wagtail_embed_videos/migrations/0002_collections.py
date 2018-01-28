282f2433007e48c61e0bc18b9809f3c5c878120a
José Luis <alagunasalahaddin@live.com>
2018-01-28 16:39:08 +0300

    Update 0002_collections.py

--- wagtail_embed_videos/migrations/0002_collections.py
+++ wagtail_embed_videos/migrations/0002_collections.py
@@ -3,7 +3,7 @@
 from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
-from wagtail.wagtailcore.models
+import wagtail.wagtailcore.models
 
 
 class Migration(migrations.Migration):
