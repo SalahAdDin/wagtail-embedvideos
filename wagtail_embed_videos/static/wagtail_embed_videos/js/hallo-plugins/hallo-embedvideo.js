7e557767a1e3c55c6c85e86a1f4d5ddba3d0fe99
José Luis <alagunasalahaddin@live.com>
2018-01-28 23:46:58 +0300

    Create hallo-embedvideo.js
    Create in order to update the hallo editor's implementation.

---
+++ wagtail_embed_videos/static/wagtail_embed_videos/js/hallo-plugins/hallo-embedvideo.js
@@ -0,0 +1,47 @@
+(function() {
+    (function($) {
+        return $.widget('IKS.hallowembedvideo', {
+            options: {
+                uuid: '',
+                editable: null
+            },
+            populateToolbar: function(toolbar) {
+                var button, widget;
+
+                widget = this;
+                button = $('<span class="' + this.widgetName + '"></span>');
+                button.hallobutton({
+                    uuid: this.options.uuid,
+                    editable: this.options.editable,
+                    label: 'Videos',
+                    icon: 'icon-media',
+                    command: null
+                });
+                toolbar.append(button);
+                return button.on('click', function(event) {
+                    var insertionPoint, lastSelection;
+
+                    lastSelection = widget.options.editable.getSelection();
+                    insertionPoint = $(lastSelection.endContainer).parentsUntil('.halloeditor').last();
+                    return ModalWorkflow({
+                        url: window.chooserUrls.embedVideoChooser + '?select_format=true',
+                        responses: {
+                            embedVideoChosen: function(embedVideoData) {
+                                var elem;
+
+                                elem = $(embedVideoData.html).get(0);
+                                lastSelection.insertNode(elem);
+                                if (elem.getAttribute('contenteditable') === 'false') {
+                                    insertRichTextDeleteControl(elem);
+                                }
+
+                                return widget.options.editable.element.trigger('change');
+                            }
+                        }
+                    });
+                });
+            }
+        });
+    })(jQuery);
+
+}).call(this);
