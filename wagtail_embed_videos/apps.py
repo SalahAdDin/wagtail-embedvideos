ffb7b72ffdc55e80657d8b613d858fd18bc8fff3
José L. S. A <alagunasalahaddin@live.com>
2018-05-19 01:56:22 +0300

    Revert "Added missing locale strings."
    This reverts commit 252e2b311ab259a8872d6026c62a839464967be4.

--- wagtail_embed_videos/apps.py
+++ wagtail_embed_videos/apps.py
@@ -1,8 +1,7 @@
 from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as _
 
 
 class WagtailEmbedVideosAppConfig(AppConfig):
     name = 'wagtail_embed_videos'
     label = 'wagtail_embed_videos'
-    verbose_name = _("Wagtail Embed Videos")
+    verbose_name = "Wagtail Embed Videos"
