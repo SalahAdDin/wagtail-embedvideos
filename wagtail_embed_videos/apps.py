252e2b311ab259a8872d6026c62a839464967be4
José L. S. A <alagunasalahaddin@live.com>
2018-05-19 01:56:22 +0300

    Added missing locale strings.

--- wagtail_embed_videos/apps.py
+++ wagtail_embed_videos/apps.py
@@ -1,7 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class WagtailEmbedVideosAppConfig(AppConfig):
     name = 'wagtail_embed_videos'
     label = 'wagtail_embed_videos'
-    verbose_name = "Wagtail Embed Videos"
+    verbose_name = _("Wagtail Embed Videos")
